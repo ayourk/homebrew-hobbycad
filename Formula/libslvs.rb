a723b2c4dc33dcd26a90e14934acdbcbc70df678
Aaron Yourk <ayourk@gmail.com>
2026-02-23 12:33:19 -0700

    libslvs: add patch to guard solvespace target references behind ENABLE_GUI
    Fixes arm64 build failure where CMake errors with 'No TARGET solvespace'
    when building with ENABLE_GUI=OFF.
    
    Signed-off-by: Aaron Yourk <ayourk@gmail.com>

--- Formula/libslvs.rb
+++ Formula/libslvs.rb
@@ -7,11 +7,18 @@ class Libslvs < Formula
   license "GPL-3.0-only"
 
   depends_on "cmake" => :build
+  depends_on "eigen"
 
   # Handle missing .git directory when building from tarball
   patch do
     url "https://raw.githubusercontent.com/ayourk/hobbycad-vcpkg/main/ports/libslvs/0001-handle-missing-git-directory.patch"
     sha256 "07c85c6f8b2468cce466675d9e1c58aa2cd5ddb6201a2ee75c124dafcd4d3182"
+  end
+
+  # Guard macOS solvespace target references behind ENABLE_GUI
+  patch do
+    url "https://raw.githubusercontent.com/ayourk/hobbycad-vcpkg/main/ports/libslvs/0002-guard-macos-solvespace-target-references.patch"
+    sha256 "9fed7c85bf8e0bbc4cdb7584bfdbed89212ad2caec6b7c5531b7990191d34821"
   end
 
   def install
@@ -24,12 +31,17 @@ class Libslvs < Formula
       end
     end
 
+    # Link Eigen headers into the expected extlib/eigen location
+    eigen_dir = buildpath/"extlib/eigen"
+    eigen_dir.mkpath
+    (eigen_dir/"Eigen").make_symlink Formula["eigen"].opt_prefix/"include/eigen3/Eigen"
+    (eigen_dir/"unsupported").make_symlink Formula["eigen"].opt_prefix/"include/eigen3/unsupported"
+
     system "cmake", "-S", ".", "-B", "build",
            *std_cmake_args,
            "-DBUILD_LIB=ON",
-           "-DBUILD_GUI=OFF",
-           "-DBUILD_CLI=OFF",
            "-DENABLE_GUI=OFF",
+           "-DENABLE_CLI=OFF",
            "-DENABLE_OPENMP=OFF",
            "-DENABLE_TESTS=OFF"
     system "cmake", "--build", "build"
